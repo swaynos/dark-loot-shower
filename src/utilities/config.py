a8f011fcf73221178e246d18f9883b5c5d7ad723
John Paul Swaynos <jpswaynos@gmail.com>
2024-08-14 16:15:03 -0700

    Need to update the app name to be the streaming client.

--- src/utilities/config.py
+++ src/utilities/config.py
@@ -1,5 +1,5 @@
 # global static config
-APP_NAME= "Steam"
+APP_NAME= "steamstreamingclient"
 APP_HEADER_HEIGHT = 56 #28 or 56, depends on DPI settings and monitor
 APP_RESIZE_REQUIRED = False
 
@@ -8,7 +8,7 @@ SCREENSHOTS_DIR = "screenshots/"
 # This will save the screenshot response from Ollama only if the image has been saved
 SAVE_SCREENSHOT_RESPONSE = True
 
-LOG_LEVEL = "INFO"
+LOG_LEVEL = "DEBUG"
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
 OLLAMA_MODEL = "llava"
