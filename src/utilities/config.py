ad60dcbc4aa80482cb6b36e7b51b84d0549566e9
John Paul Swaynos <jpswaynos@gmail.com>
2024-11-08 08:49:09 -0800

    Added moonlight as an option. Honestly, I should move some of these values out into .env

--- src/utilities/config.py
+++ src/utilities/config.py
@@ -1,12 +1,12 @@
 # global static config
-APP_NAME = "NVIDIA GeForce NOW" #"steamstreamingclient"
-APP_HEADER_HEIGHT = 0 #28 or 56, depends on DPI settings and monitor
+APP_NAME = "Moonlight" #"NVIDIA GeForce NOW" "steamstreamingclient"
+APP_HEADER_HEIGHT = 0 #0 if fullscreen, 28 or 56 depending on DPI settings and monitor
 APP_RESIZE_REQUIRED = False
 
-CAPTURE_IMAGE_DELAY = 1 # Delay in seconds between loops of capture_image_handler
+CAPTURE_IMAGE_DELAY = 5 # Delay in seconds between loops of capture_image_handler
 
 SAVE_SCREENSHOTS = True
-SCREENSHOTS_DIR = "screenshots/"
+SCREENSHOTS_DIR = "./screenshots/"
 # This will save the screenshot response from Ollama only if the image has been saved
 SAVE_SCREENSHOT_RESPONSE = True
 
